d608f7df1e9353376a5edad5a36201947313e71f
wunder957 <wakedwunder957@gmail.com>
2023-08-22 17:12:29 +0800

    docs/examples (#14)
    * Init commit for examples
    
    * Add default config
    
    * Fix soft link to default config
    
    * Rename SQLiteCollector
    
    * Add default config
    
    * Raise NotImplementedError when call base Filter()
    
    * Add Examples

---
+++ examples/extension/collector-extension/duetector_echocollector/echo.py
@@ -0,0 +1,25 @@
+from typing import Any, Dict, Optional
+
+from duetector.collectors import Collector
+from duetector.collectors.models import Tracking
+from duetector.extension.collector import hookimpl
+
+
+class EchoCollector(Collector):
+    default_config = {
+        "disabled": False,
+    }
+
+    def __init__(self, config: Optional[Dict[str, Any]] = None, *args, **kwargs):
+        super().__init__(config, *args, **kwargs)
+
+    def _emit(self, t: Tracking):
+        print(t)
+
+    def summary(self) -> Dict:
+        return {}
+
+
+@hookimpl
+def init_collector(config):
+    return EchoCollector(config)
---
+++ examples/extension/filter-extension/duetector_echofilter/echo.py
@@ -0,0 +1,19 @@
+from typing import NamedTuple, Optional
+
+from duetector.extension.filter import hookimpl
+from duetector.filters import Filter
+
+
+class EchoFilter(Filter):
+    default_config = {
+        "disabled": False,
+    }
+
+    def __call__(self, data: NamedTuple) -> Optional[NamedTuple]:
+        print(data)
+        return data
+
+
+@hookimpl
+def init_filter(config):
+    return EchoFilter(config)
