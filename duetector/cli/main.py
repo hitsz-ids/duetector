031df243238b7b733da8087b642ec9f925b6ee65
wunder957 <wakedwunder957@gmail.com>
2023-08-22 12:30:30 +0800

    Add CLI Entrypoint

--- duetector/cli/main.py
+++ duetector/cli/main.py
@@ -1,1 +1,18 @@
 import click
+
+
+@click.command()
+def monitor():
+    click.echo("Monitoring")
+
+
+@click.group()
+def cli():
+    pass
+
+
+# cli.add_command()
+cli.add_command(monitor)
+
+if __name__ == "__main__":
+    cli()
