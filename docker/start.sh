b51b918085ef1bf9948a07a19097f05c8560b53b
wunder957 <wakedwunder957@gmail.com>
2023-08-29 16:17:29 +0800

    Fix script

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-duectl-daemon start --log-level=DEBUG
+duectl-daemon start --loglevel=DEBUG
 
 cd /home/application
 export PATH=/home/application/.local/bin:$PATH
-exec su "application" -c "jupyter-lab"
+exec su "application" -c "jupyter-lab --ip=0.0.0.0 --port=8888 --no-browser"
