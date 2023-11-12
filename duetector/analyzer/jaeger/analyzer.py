8cb41fe9eafafb6a0f4f0c58f5b64903e219e85c
wunder957 <wakedwunder957@gmail.com>
2023-11-12 09:38:01 +0800

    Switch to otel build proto

--- duetector/analyzer/jaeger/analyzer.py
+++ duetector/analyzer/jaeger/analyzer.py
@@ -2,16 +2,29 @@ import asyncio
 
 import grpc
 
+from duetector.analyzer.base import Analyzer
 from duetector.analyzer.jaeger.proto.query_pb2 import *
 from duetector.analyzer.jaeger.proto.query_pb2_grpc import *
+from duetector.extension.analyzer import hookimpl
 
 
-async def run() -> None:
-    async with grpc.aio.insecure_channel("localhost:16685") as channel:
-        stub = QueryServiceStub(channel)
-        response = await stub.GetServices(GetServicesRequest())
-        print(response)
+class JaegerAnalyzer(Analyzer):
+    default_config = {
+        "disabled": True,
+    }
+
+
+@hookimpl
+def init_analyzer(config):
+    return JaegerAnalyzer(config)
 
 
 if __name__ == "__main__":
+
+    async def run() -> None:
+        async with grpc.aio.insecure_channel("localhost:16685") as channel:
+            stub = QueryServiceStub(channel)
+            response = await stub.GetServices(GetServicesRequest())
+            print(response)
+
     asyncio.run(run())
--- duetector/analyzer/jaeger/proto/query_pb2.py
+++ duetector/analyzer/jaeger/proto/query_pb2.py
@@ -14,10 +14,21 @@ _sym_db = _symbol_database.Default()
 
 from google.protobuf import duration_pb2 as google_dot_protobuf_dot_duration__pb2
 from google.protobuf import timestamp_pb2 as google_dot_protobuf_dot_timestamp__pb2
+from opentelemetry.exporter.jaeger.proto.grpc.gen import model_pb2 as model__pb2
+from opentelemetry.exporter.jaeger.proto.grpc.gen.gogoproto import (
+    gogo_pb2 as gogoproto_dot_gogo__pb2,
+)
+from opentelemetry.exporter.jaeger.proto.grpc.gen.google.api import (
+    annotations_pb2 as google_dot_api_dot_annotations__pb2,
+)
 
-from . import model_pb2 as model__pb2
-from .gogoproto import gogo_pb2 as gogoproto_dot_gogo__pb2
-from .google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
+# from . import model_pb2 as model__pb2
+
+
+# from .gogoproto import gogo_pb2 as gogoproto_dot_gogo__pb2
+
+
+# from .google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 
 DESCRIPTOR = _descriptor.FileDescriptor(
     name="query.proto",
