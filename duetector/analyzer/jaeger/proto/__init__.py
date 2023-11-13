243b7dfa0c379fb4e62ed053cc29d379df1d65db
wunder957 <wakedwunder957@gmail.com>
2023-11-13 17:36:11 +0800

    Fixing typo

--- duetector/analyzer/jaeger/proto/__init__.py
+++ duetector/analyzer/jaeger/proto/__init__.py
@@ -1,7 +1,7 @@
-# Fix invalid proto descriptor for file "model.proto"
 import sys
 from os.path import dirname
 
+# Fix invalid proto descriptor for file "model.proto"
 import opentelemetry.exporter.jaeger.proto.grpc  # noqa
 
 sys.path.append(dirname(__file__))
