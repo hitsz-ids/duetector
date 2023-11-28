0e61fdbc900e32a07b6865085f80bac0283f0b6b
wunder957 <wakedwunder957@gmail.com>
2023-11-28 17:11:44 +0800

    Add _cache for ProcWatcher

--- duetector/injectors/inspector.py
+++ duetector/injectors/inspector.py
@@ -1,14 +1,34 @@
 from __future__ import annotations
 
+import glob
 import itertools
 import signal
 from threading import Event, Thread
 from typing import Any
 
-from watchfiles import watch
+import pydantic
+from watchfiles import Change, DefaultFilter, _rust_notify, watch
+
+try:
+    from functools import cache
+except ImportError:
+    from functools import lru_cache as cache
 
 from duetector.log import logger
 from duetector.utils import Singleton
+
+
+class ProcInfo(pydantic.BaseModel):
+    pass
+
+
+class PidFilter(DefaultFilter):
+    @classmethod
+    def get_pid(cls, path: str) -> str:
+        return path.split("/")[-1]
+
+    def __call__(self, change: Change, path: str) -> bool:
+        return super().__call__(change, path) and self.get_pid(path).isnumeric()
 
 
 class ProcWatcher(metaclass=Singleton):
@@ -20,6 +40,7 @@ class ProcWatcher(metaclass=Singleton):
         self.proc_dir = proc_dir
         self.ignore_permission_denied = ignore_permission_denied
 
+        self._cache: dict[int, ProcInfo] = {}
         self.thread: Thread | None = None
 
         self.stop_event = Event()
@@ -28,6 +49,19 @@ class ProcWatcher(metaclass=Singleton):
         self.start()
         signal.signal(signal.SIGINT, self.stop)
         signal.signal(signal.SIGTERM, self.stop)
+
+    def _sync(self):
+        logger.debug("Force sync proc cache.")
+        pids = [
+            int(PidFilter.get_pid(path))
+            for path in glob.glob(f"{self.proc_dir.rstrip('/')}/[0-9]*")
+        ]
+        for pid in pids:
+            if pid not in self._cache:
+                self.add_cache(pid)
+        for cached_pid in self._cache.keys():
+            if cached_pid not in pids:
+                self.remove_cache(cached_pid)
 
     def start(self):
         if self.thread:
@@ -43,9 +77,10 @@ class ProcWatcher(metaclass=Singleton):
             while True:
                 try:
                     self._watch()
+                except _rust_notify.WatchfilesRustInternalError as e:
+                    self._sync()
                 except Exception as e:
                     logger.exception(e)
-                self.stop_event.wait(0.001)
                 if self.stop_event.is_set():
                     return
 
@@ -53,21 +88,46 @@ class ProcWatcher(metaclass=Singleton):
             target=_,
         )
         self.thread.start()
+        self._sync()
         logger.info("Proc watcher started.")
 
     def _watch(self):
         for changes in watch(
             self.proc_dir,
+            watch_filter=PidFilter(),
             stop_event=self.stop_event,
             ignore_permission_denied=self.ignore_permission_denied,
             recursive=False,
+            poll_delay_ms=300,
             force_polling=True,
         ):
-            pass
-            # logger.info(changes)
+            for change, path in changes:
+                pid = int(PidFilter.get_pid(path))
+                if change == Change.added:
+                    self.add_cache(pid)
+                elif change == Change.deleted:
+                    self.remove_cache(pid)
+
+    def get(self, pid: int) -> ProcInfo | None:
+        return self._cache.get(pid)
+
+    def add_cache(self, pid: int):
+        if pid in self._cache:
+            return
+        # TODO: Cache it
+        logger.debug(f"Add proc cache for `{pid}`")
+        pass
+
+    def remove_cache(self, pid: int, delay=1):
+        if not delay:
+            logger.debug(f"Remove proc cache for `{pid}` now")
+            return self._cache.pop(pid, None)
+        # TODO: Schedule remove cache after 1 seconds
+        logger.debug(f"Remove proc cache for `{pid}` after {delay} seconds")
 
     def stop(self, sig=None, frame=None):
         self.stop_event.set()
+        self._cache.clear()
         if self.thread:
             logger.info("Waiting proc watcher to stop.")
             self.thread.join(1)
@@ -90,9 +150,11 @@ class Inspector:
     name = None
 
     @property
+    @cache
     def id(self) -> str:
         return with_prefix(self.sep, self.prefix, self.name or self.__class__.__name__)
 
+    @cache
     def with_id(self, key: str | list[str]) -> str:
         return with_prefix(self.sep, self.id, key)
 
@@ -124,6 +186,7 @@ class Inspector:
 
 class NamespaceInspector(Inspector):
     @property
+    @cache
     def name(self) -> str:
         return self.sep.join(["proc", "namespace"])
 
@@ -139,6 +202,7 @@ class NamespaceInspector(Inspector):
 
 class CgroupInspector(Inspector):
     @property
+    @cache
     def name(self) -> str:
         return self.sep.join(["proc", "cgroup"])
 
